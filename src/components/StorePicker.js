7d82e68af6a1b173bfee09d035671dffeea3ec96
Nick Webb <nickwebb1@gmail.com>
2023-01-05 11:31:08 +0000

    Progress to date on Beginner React course

---
+++ src/components/StorePicker.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+  goToStore() {
+    console.log('Going to store');
+  }
+  render() {
+    console.log(this);
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
